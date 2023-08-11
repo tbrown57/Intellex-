d726fac6e6bf72146fddfe86bd273c00c8c22c21
Tim Brown <timothy.brown.57@outlook.com>
2023-08-11 00:11:47 -0400

    Customer Record Buildout

--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Record (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    industry = models.CharField(max_length=50)
+    business_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to See if Loggin In
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +23,7 @@ def home(request):
             messages.success(request, "There Was An Error")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -29,4 +33,29 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Are Logged In")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "YOU MUST BE LOGGED IN")
+        return redirect('home')
